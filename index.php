b7dd38962ed0617b810dd2043b9dc5d11aa922f7
Longinus <jesuscaesar@outlook.com>
2021-12-25 15:21:56 +1000

    Latest update

--- index.php
+++ index.php
@@ -171,7 +171,7 @@ input, select, textarea {
 <?php }} ?>
 </p>
 <?php } else { ?>
-    <h2 align='center'><img style="height:92px;position:relative;" src="<?=$pkgLaunchIcon;?>"> <?=$pkgLaunchTitle;?></h2>
+    <h2 align='center'><img style="height:92px;position:relative;" src="<?=$pkgLaunchCover;?>"> <?=$pkgLaunchTitle;?></h2>
     <p align='center'><?=$phpLaunchAuthor;?></p>
     <p align='center'><input class='actionButton' type='button' value="Play" onclick="window.location.href='<?=$pkgLaunchApp;?>';"></p>
 <?php } ?>
